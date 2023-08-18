42d036409f7f28f6874900d4d26698d7c04056d6
nichol6s <nicholas.jake15@gmail.com>
2023-08-18 10:24:12 -0300

    adicionando comentarios ao exercicio3.js

--- exercicio-03-arrays/js/exercicio3.js
+++ exercicio-03-arrays/js/exercicio3.js
@@ -1,12 +1,15 @@
+//Declarando o array "taskList" que vai armazenar as tarefas
 let taskList = [];
 
-const taskForm = document.querySelector("#task-form");
-const taskTable = document.querySelector("#task-table");
-const taskListContainer = document.querySelector("#task-list");
+//Aqui estamos selecionando os elementos html pelo seu Id
+const taskForm = document.querySelector("#task-form"); //Representa o formulário de adição de tarefas
+const taskTable = document.querySelector("#task-table"); //Tabela que irá mostrar as tarefas
+const taskListContainer = document.querySelector("#task-list"); //Corpo da tabela onde as linhas de tarefa serão adicionadas.
 
 taskForm.addEventListener("submit", (evt) => {
     evt.preventDefault();
 
+    //Aqui estamos coletando os valores dos campos do formulario
     const taskDescription = document.querySelector("#task-description").value;
     const taskAuthor = document.querySelector("#task-author").value;
     const taskDepartment = document.querySelector("#task-department").value;
@@ -14,6 +17,7 @@ taskForm.addEventListener("submit", (evt) => {
     const taskValue = document.querySelector("#task-value").value;
     const taskDuration = document.querySelector("#task-duration").value;
 
+    //Esse task vai ser adicionado ao array
     const task = {
         description: taskDescription,
         author: taskAuthor,
@@ -23,6 +27,7 @@ taskForm.addEventListener("submit", (evt) => {
         duration: taskDuration
     };
 
+    //A função update é chamada pra atualizar a tabela de tarefas na interface
     taskList.push(task);
     updateTaskTable();
     taskForm.reset();
@@ -30,9 +35,11 @@ taskForm.addEventListener("submit", (evt) => {
 
 function updateTaskTable() {
     taskListContainer.innerHTML = "";
-
+    
+    //Criando um forEach pra cada elemento do Array
     taskList.forEach((task, index) => {
-        const row = document.createElement("tr");
+        const row = document.createElement("tr"); //Criando uma constante pra criar a tag la no html
+        //Aqui a gnt pega essa const e com cada elemento do Array a gnt cria as tags
         row.innerHTML = `
             <td>${task.description}</td>
             <td>${task.author}</td>
